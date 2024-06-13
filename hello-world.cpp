f353b30233db4a8a76721b3bcdfeffbb1b99c4f7
Dalton Messmer <messmer.dalton@gmail.com>
2024-06-12 22:54:19 -0400

    Alsa test (WIP)

--- hello-world.cpp
+++ hello-world.cpp
@@ -1,6 +1,12 @@
 #include <iostream>
+#include <alsa/asoundlib.h>
 
 int main()
 {
 	std::cout << "Hello, World!\n";
+
+	auto format = SND_PCM_FORMAT_UNKNOWN;
+	size_t samples = 0;
+
+	snd_pcm_format_size(format, samples);
 }
